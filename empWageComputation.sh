2d0cf665be3383987a2b1735aad338596eefa619
unknown <patilsharad2000@gmail.com>
2021-01-10 15:33:05 +0530

    check Employee Atttendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee with Wage Computation "
+isPresent=1
+WagePerHour=20
+noOfHour=8
+checkAttendance=$((RANDOM%3))
+
+if [ $isPresent -eq $checkAttendance ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee Absent"
+fi
+
