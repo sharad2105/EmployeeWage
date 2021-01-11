636356c3eda1e490fdf80f43e28ec2b8d6a00a12
unknown <patilsharad2000@gmail.com>
2021-01-11 09:35:11 +0530

    Calculate Part Time Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,25 @@
 
 echo "Welcome to Employee with Wage Computation "
 isPresent=1
+isPartTime=2
 WagePerHour=20
 NoOfHour=8
-checkAttendance=$((RANDOM%2))
+PartTimeHour=4
+checkAttendance=$((RANDOM%3))
 
 if [ $isPresent -eq $checkAttendance ]
 then
         echo "Employee is Present"
 	payment=$(($WagePerHour*$NoOfHour))
 	echo "payment"$payment
+elif [ $isPartTime -eq $checkAttendance ]
+then
+	echo "Employee is Part Time"
+	payment=$(($WagePerHour*$PartTimeHour))
+	echo "Part Time Payment"$payment
 else
         echo "Employee Absent"
+	echo "Payment is 0"
+	echo "Part Time Payment is 0"
 fi
 
